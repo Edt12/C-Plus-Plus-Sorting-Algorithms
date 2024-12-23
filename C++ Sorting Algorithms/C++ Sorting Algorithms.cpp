42d87964581368e6be92655349b0e40aca790e36
Edt12 <104518243+Edt12@users.noreply.github.com>
2024-12-23 11:51:48 +0000

    Finished Insertion Sort

--- C++ Sorting Algorithms/C++ Sorting Algorithms.cpp
+++ C++ Sorting Algorithms/C++ Sorting Algorithms.cpp
@@ -4,13 +4,32 @@
 using namespace ::std;
 
 vector<int> insertionSort(vector<int> input) {
+    //start at 1 as insertion sort works by comparing elem to all previous elem
+    for (int j = 1; j < input.size();j++) {
+        int key = input[j];
+        int i = j - 1;
+        while (i >= 0 && input[i] > key) {
+            //changes index to insert the current elem (Key)
+            input[i + 1] = input[i];
+            i = i - 1;
+
+        }
+        //+1 as if the while is true then could end up with i being equal to -1
+        input[i + 1] = key;
+    }
     return input;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    vector<int> unsortedArray = { 5,4,3,2,1,0 };
-    insertionSort(unsortedArray);
+    vector<int> unsortedArray = { 5,41,3,27,2,0 };
+    vector<int> output = insertionSort(unsortedArray);
+    for (int i = 0; i < output.size(); i++)
+    {
+        cout << output[i] << endl;
+
+    }
+
+
 }
 
